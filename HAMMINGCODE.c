188ac9b02835e87da337827a3320e2a24976ffca
kuman514 <7iggif@naver.com>
2016-02-23 22:53:24 +0900

    HAMMINGCODE Solving. Not finished.

--- HAMMINGCODE.c
+++ HAMMINGCODE.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
 
+typedef union _CODE
+{
+	char message;
+	char space:1, b1:1, b2:1, b3:1, b4:1, b5:1, b6:1, b7:1;
+} code;
+
+char stom(const char *input)
+{
+	char result = 0;
+	int i;
+	for(i = 0; i < 7; ++i)
+	{
+		result <<= 1;
+		result |= input[i] - '0';
+	}
+	return result;
+}
+
 int main(void)
 {
 	int T = 0;
 	int i, j;
 	while(T < 1) scanf("%d", &T);
 
-	char line[T][8];
+	code line[T];
+	char input[8];
+
 	for(i = 0; i < T; ++i)
 	{
-		scanf("%s", line[i]);
-		for(j = 0; j < 7; ++j)
-		{
-			if(line[i][j] != '0')
-				line[i][j] = 1;
-			else
-				line[i][j] = 0;
-		}
+		scanf("%s", input);
+		line[i] = stom(input);
 	}
-	// turned into 0 and 1
 
 	return 0;
 }
