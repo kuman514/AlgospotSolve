c444be049aa778f2fa4a1a2b45e74d34bed0baaf
kuman514 <7iggif@naver.com>
2016-02-24 16:33:26 +0900

    HAMMINGCODE Solving. Not finished.

--- HAMMINGCODE.c
+++ HAMMINGCODE.c
@@ -1,42 +1,37 @@
 #include <stdio.h>
-
-typedef union _CODE
-{
-	char message;
-	char space:1, b1:1, b2:1, b3:1, b4:1, b5:1, b6:1, b7:1;
-} code;
-
-char stom(const char *input)
-{
-	char result = 0;
-	int i;
-	for(i = 0; i < 7; ++i)
-	{
-		result <<= 1;
-		result |= input[i] - '0';
+int main(){
+	int T=0,i,j;
+	// insert T lines
+	while(T<1) scanf("%d",&T);
+	char code[T][8];
+	char error[T],errpos[T];
+	for(i=0;i<T;++i){
+		error[i]=0;
+		errpos[i]=0;
+		scanf("%s",code[i]);
+		for(j=0;j<7;++j)
+			code[i][j]-='0';
 	}
-	return result;
-}
-
-void printbit(code input)
-{
-	printf("%d%d%d%d%d%d%d\n", input.b1, input.b2, input.b3, input.b4, input.b5, input.b6, input.b7);
-}
-
-int main(void)
-{
-	int T = 0;
-	int i, j;
-	while(T < 1) scanf("%d", &T);
-
-	code line[T];
-	char input[8];
-
-	for(i = 0; i < T; ++i)
-	{
-		scanf("%s", input);
-		line[i].message = stom(input);
+	// check errors
+	for(i=0;i<T;++i){
+		if(code[i][0]!=code[i][2]^code[i][4]^code[i][6]){
+			++error[i];errpos[i]=2;
+		}
+		if(code[i][1]!=code[i][2]^code[i][5]^code[i][6]){
+			++error[i];errpos[i]=1;
+		}
+		if(code[i][2]!=code[i][4]^code[i][5]^code[i][6]){
+			++error[i];errpos[i]=0;
+		}
 	}
-
+	// decode message
+	for(i=0;i<T;++i){
+		if(error[i]<1)
+			printf("%d%d%d%d\n",code[i][2],code[i][4],code[i][5],code[i][6]);
+		else if(error[i]==1){
+			printf("%d%d%d%d\n",code[i][2],code[i][4],code[i][5],code[i][6]);
+		}
+		else printf("ERROR\n");
+	}
 	return 0;
 }
