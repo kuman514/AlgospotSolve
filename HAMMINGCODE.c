2e8e45e86f82fc1ad13fdd6934473f85ecfa21f7
kuman514 <7iggif@naver.com>
2016-02-24 22:55:46 +0900

    HAMMINGCODE Solving. Finished, but maybe not accurate.

--- HAMMINGCODE.c
+++ HAMMINGCODE.c
@@ -1,37 +1,28 @@
 #include <stdio.h>
 int main(){
 	int T=0,i,j;
-	// insert T lines
 	while(T<1) scanf("%d",&T);
-	char code[T][8];
-	char error[T],errpos[T];
+	char code[T][8],syndrome[T][3];
+	//char error[T],errpos[T];
 	for(i=0;i<T;++i){
-		error[i]=0;
-		errpos[i]=0;
+		//error[i]=0;
+		//errpos[i]=0;
 		scanf("%s",code[i]);
 		for(j=0;j<7;++j)
 			code[i][j]-='0';
 	}
-	// check errors
 	for(i=0;i<T;++i){
-		if(code[i][0]!=code[i][2]^code[i][4]^code[i][6]){
-			++error[i];errpos[i]=2;
-		}
-		if(code[i][1]!=code[i][2]^code[i][5]^code[i][6]){
-			++error[i];errpos[i]=1;
-		}
-		if(code[i][2]!=code[i][4]^code[i][5]^code[i][6]){
-			++error[i];errpos[i]=0;
-		}
+		syndrome[i][2]=code[i][0]^code[i][2]^code[i][4]^code[i][6];
+		syndrome[i][1]=code[i][1]^code[i][2]^code[i][5]^code[i][6];
+		syndrome[i][0]=code[i][3]^code[i][4]^code[i][5]^code[i][6];
+		code[i][0]|=syndrome[i][0];
+		code[i][1]|=syndrome[i][1];
+		code[i][2]|=syndrome[i][2];
 	}
-	// decode message
 	for(i=0;i<T;++i){
-		if(error[i]<1)
-			printf("%d%d%d%d\n",code[i][2],code[i][4],code[i][5],code[i][6]);
-		else if(error[i]==1){
-			printf("%d%d%d%d\n",code[i][2],code[i][4],code[i][5],code[i][6]);
-		}
-		else printf("ERROR\n");
+		printf("%d%d%d%d\n",code[i][2],code[i][4],code[i][5],code[i][6]);
+		//printf("%d%d%d%d\n",code[i][2],code[i][4],code[i][5],code[i][6]);
+		//else printf("ERROR\n");
 	}
 	return 0;
 }
