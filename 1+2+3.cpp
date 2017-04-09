f5588af010f6580e11e5703719c4e4a57017da0a
kuman514 <7iggif@naver.com>
2017-04-10 02:20:16 +0900

    the number of ways to express number N by sum of 1,2,3

---
+++ 1+2+3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <stack>
+
+int main(void)
+{
+	int c;
+	scanf("%d", &c);
+
+	int n[c], a[c] = {0,};
+	for(int i = 0; i < c; i++) scanf("%d", &n[i]);
+
+	for(int i = 0; i < c; i++)
+	{
+		std::stack<int> p;
+		for(int j = 1; j <= 3; j++) p.push(j);
+
+		while(!(p.empty()))
+		{
+			int s = p.top();
+			p.pop();
+			
+			if(s == n[i])
+				a[i] += 1;
+			else if(s < n[i])
+				for(int j=1; j<=3; j++) p.push(s+j);
+		}
+	}
+
+	for(int i = 0; i < c; i++) printf("%d\n", a[i]);
+	
+	return 0;
+}
